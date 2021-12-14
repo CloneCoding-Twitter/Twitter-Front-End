ff2198a61bfe370469b2c4f1f7937d00842b8676
가즈아 <wjsdydxo45@naver.com>
2021-12-15 02:48:09 +0900

    login-success

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import { Button, Grid } from "../elements";
+import { actionCreators as userActions } from "../redux/modules/user";
+import { useDispatch } from "react-redux";
 import { createMuiTheme, ThemeProvider, TextField } from "@material-ui/core";
 
 const theme = createMuiTheme({
@@ -16,45 +18,83 @@ const theme = createMuiTheme({
 });
 
 const validationSchema = yup.object({
-  email: yup
-    .string("이메일을 입력해주세요.")
-    .email("이메일을 입력해주세요.")
-    .required("이메일 입력은 필수입니다."),
+  loginId: yup
+    .string("아이디를 입력해주세요.")
+    .min(3, "아이디는 최소 3자 이상이어야 합니다.")
+    .max(12, "아이디는 최대 12자 이하이어야 합니다.")
+    .required("아이디 입력은 필수입니다.")
+    .matches(/[a-z]+[A-Z0-9]*$/, "영대소문, 숫자 혼합 3~12자 입력해주세요."),
+  nickname: yup
+    .string("닉네임을 입력해주세요.")
+    .min(3, "닉네임은 최소 8자 이상이어야 합니다.")
+    .max(6, "닉네임은 최대 6자 이하이어야 합니다.")
+    .required("닉네임 입력은 필수입니다.")
+    .matches(/[a-z]+[A-Z0-9]*$/, "영대소문, 숫자 혼합 3~6자 입력해주세요."),
   password: yup
     .string("비밀번호를 입력해주세요.")
     .min(8, "비밀번호는 최소 8자 이상이어야 합니다.")
-    .required("비밀번호 입력은 필수입니다."),
+    .max(20, "비밀번호는 최대 20자 이하이어야 합니다.")
+    .required("비밀번호 입력은 필수입니다.")
+    .matches(/[a-z]+[A-Z0-9]*$/, "영대소문, 숫자 혼합 3~20자 입력해주세요."),
+  passwordCheck: yup
+    .string()
+    .min(8, "비밀번호는 최소 8자 이상이어야 합니다.")
+    .max(20, "비밀번호는 최대 20자 이하이어야 합니다.")
+    .required("비밀번호 확인은 필수입니다.")
+    .oneOf(
+      [yup.ref("password"), null],
+      "비밀번호가 다릅니다. 다시 입력해 주세요."
+    ),
 });
 
-const LoginForm = (props) => {
+const SignUpForm = (props) => {
+  const dispatch = useDispatch();
+
+  const signup = () => {
+    console.log(signup);
+  };
+
   const formik = useFormik({
     initialValues: {
-      email: "",
+      loginId: "",
+      nickname: "",
       password: "",
+      passwordCheck: "",
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+      dispatch(userActions.signupDB(values));
     },
   });
-
-  console.log(props.onClick);
 
   return (
     <Grid is_flex justify="center">
       <ThemeProvider theme={theme}>
         <form onSubmit={formik.handleSubmit}>
-          <Grid is_flex justify="center">
+          <Grid is_flex justify="center" margin="0 0 20px 0">
             <TextField
               color="primary"
               fullWidth
-              id="email"
-              name="email"
-              label="이메일"
-              value={formik.values.email}
+              id="loginId"
+              name="loginId"
+              label="아이디"
+              value={formik.values.loginId}
               onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
+              error={formik.touched.loginId && Boolean(formik.errors.loginId)}
+              helperText={formik.touched.loginId && formik.errors.loginId}
+            />
+          </Grid>
+          <Grid is_flex justify="center" margin="20px 0">
+            <TextField
+              color="primary"
+              fullWidth
+              id="nickname"
+              name="nickname"
+              label="닉네임"
+              value={formik.values.nickname}
+              onChange={formik.handleChange}
+              error={formik.touched.nickname && Boolean(formik.errors.nickname)}
+              helperText={formik.touched.nickname && formik.errors.nickname}
             />
           </Grid>
           <Grid is_flex justify="center" margin="20px 0">
@@ -70,17 +110,36 @@ const LoginForm = (props) => {
               helperText={formik.touched.password && formik.errors.password}
             />
           </Grid>
-          
+          <Grid is_flex justify="center" margin="20px 0 30px 0">
+            <TextField
+              fullWidth
+              id="passwordCheck"
+              name="passwordCheck"
+              label="비밀번호 확인"
+              type="password"
+              value={formik.values.passwordCheck}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.passwordCheck &&
+                Boolean(formik.errors.passwordCheck)
+              }
+              helperText={
+                formik.touched.passwordCheck && formik.errors.passwordCheck
+              }
+            />
+          </Grid>
           <Grid is_flex justify="center">
             <Button
-              is_tweeterHover
+              is_blackHover
+              bg="#000"
               width="300px"
               height="42px"
-              font_color="#ffffff"
+              font_color="#fff"
               font_size="15px"
               bold="900"
+              _onClick={signup}
             >
-              로그인
+              가입하기
             </Button>
           </Grid>
         </form>
@@ -89,4 +148,4 @@ const LoginForm = (props) => {
   );
 };
 
-export default LoginForm;
+export default SignUpForm;
